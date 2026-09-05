74360a75d513cc5fe859c233c8bb99e322e836bf
PaarTechnology <yadavamit0123@gmail.com>
2026-09-05 23:28:16 +0530

    setup

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -19,7 +19,7 @@ pluginManagement {
 plugins {
     id("org.jetbrains.kotlin.android") version "2.2.0" apply false  // Updated to the latest version
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
-    id("com.android.application") version "8.7.0" apply false
+    id("com.android.application") version "8.9.2" apply false
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
 
